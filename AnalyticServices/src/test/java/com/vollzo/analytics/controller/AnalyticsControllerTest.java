21e7e212574a677a1da8450bd7fa041b3872299d
tanmaychand1983 <75400273+tanmaychand1983@users.noreply.github.com>
2021-01-29 20:37:27 +0530

    Add JUnit Testcase

--- AnalyticServices/src/test/java/com/vollzo/analytics/controller/AnalyticsControllerTest.java
+++ AnalyticServices/src/test/java/com/vollzo/analytics/controller/AnalyticsControllerTest.java
@@ -1,57 +1,57 @@
 package com.vollzo.analytics.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import com.vollzo.analytics.service.AnalyticsService;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-/**
- * {@summary}: This is the JUNIT Test class for Analytics Controller class.
- * 
- * @className: AnalyticsControllerTest
- * @author Ajaya Kar
- * @since: Jan 23, 2021
- * @version: 1.0.0
- * 
- * @revision: {Name: Date: Reason:} 
- * 
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vollzo.analytics.service.AnalyticsService;
+import com.vollzo.analytics.vo.AnalyticsRequestVO;
+import com.vollzo.analytics.vo.AnalyticsResponseVO;
 
 @WebMvcTest(AnalyticsController.class)
 public class AnalyticsControllerTest {
+	
+	@MockBean
+	private AnalyticsService analyticsService;
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
-	private AnalyticsService service;
-
-	@Test
-	public void shouldReturnStatus200ForNearMiss() throws Exception {
-		this.mockMvc.perform(get("/analytics/nearmiss")).andDo(print()).andExpect(status().isOk());
-	}
 	
 	@Test
-	public void shouldReturnStatus200ForNearMissWithParameters() throws Exception {
-		this.mockMvc.perform(get("/analytics/nearmiss?startDate=\"\"&endDate=\"\"")).andDo(print()).andExpect(status().isOk());
+	@DisplayName("NearMiss Dashboard Should list all the data")
+	public void nearMissShouldReturnDataForGivenParameter()throws Exception {
+		AnalyticsRequestVO requestVO = new AnalyticsRequestVO();
+		requestVO.setVesselIds("75");
+		requestVO.setStartDate("2020-09-16");
+		requestVO.setEndDate("2020-09-16");
+		
+		AnalyticsResponseVO responseVO = new AnalyticsResponseVO();
+		
+		Mockito.when(analyticsService.getNearMissData(requestVO)).thenReturn(responseVO);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/analytics/nearmiss/")
+					.accept(MediaType.APPLICATION_JSON_VALUE)
+					.content(asJsonString(requestVO)))
+					.andDo(print())
+					.andReturn();
 	}
 	
-	@Test
-	public void shouldReturnStatus404ForNearMiss() throws Exception {
-		this.mockMvc.perform(get("/analytics/nearmiss1")).andDo(print()).andExpect(status().is(404));
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
-	@Test
-	public void shouldReturnStatus200ForAccidentIncident() throws Exception {
-		this.mockMvc.perform(get("/analytics/accidentincident")).andDo(print()).andExpect(status().isOk());
-	}
-	
-	@Test
-	public void shouldReturnStatus404ForAccidentIncident() throws Exception {
-		this.mockMvc.perform(get("/analytics/accidentincident1")).andDo(print()).andExpect(status().is(404));
-	}
 }
